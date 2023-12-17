b8f45ad23f5a68f5b82b81d6328f467303fcfdd9
Linh Nguyen Le Thanh <linhnlt@nal.vn>
2023-12-17 11:02:25 +0700

    Serializers In Django REST Framework | Learn Django REST Framework #3

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-from django.apps import AppConfig
 
+"""
+class Post
+    id : int
+    title: str
+    content: str
+    date: datetime
+"""
 # Create your models here.
 
-class Post(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'posts'
+class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
--- posts/views.py
+++ posts/views.py
@@ -2,31 +2,32 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        "id": 1,
-        "title": "Post 1",
-        "content": "This is the first post",
-        "date": "2021-09-10",
-        "author": "John Doe"
-    },
-    {
-        "id": 2,
-        "title": "Post 2",
-        "content": "This is the second post",
-        "date": "2021-09-10",
-        "author": "John Doe"
-    },
-    {
-        "id": 3,
-        "title": "Post 3",
-        "content": "This is the third post",
-        "date": "2021-09-10",
-        "author": "John Doe"
-    }
-    ]
+# posts = [
+#     {
+#         "id": 1,
+#         "title": "Post 1",
+#         "content": "This is the first post",
+#         "date": "2021-09-10",
+#         "author": "John Doe"
+#     },
+#     {
+#         "id": 2,
+#         "title": "Post 2",
+#         "content": "This is the second post",
+#         "date": "2021-09-10",
+#         "author": "John Doe"
+#     },
+#     {
+#         "id": 3,
+#         "title": "Post 3",
+#         "content": "This is the third post",
+#         "date": "2021-09-10",
+#         "author": "John Doe"
+#     }
+#     ]
 
 @api_view(["GET", "POST"])
 def home(request:Request):
@@ -41,12 +42,13 @@ def home(request:Request):
 
 @api_view(["GET"])
 def index(request:Request):
+    posts = Post.objects.all()
     return Response({"data": posts, "status":status.HTTP_200_OK})
 
 
 @api_view(["GET"])
 def detail(request:Request, id:int):
-    post = posts[id]
-    if posts:
+    post = Post.objects.get(id=id)
+    if post:
         return Response({"data": post, "status": status.HTTP_200_OK})
     return Response({data: "Post not found", "status":status.HTTP_404_NOT_FOUND})
